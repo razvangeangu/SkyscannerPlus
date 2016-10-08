9d333be3e075203038bafe47bc4d38f2cc8cf898
Razvan-Gabriel Geangu <k1502791@kcl.ac.uk>
2016-10-08 22:04:17 +0200

    fixed smartana

--- js/smartana.js
+++ js/smartana.js
@@ -1,3 +1,5 @@
+var uniqueID = null;
+
 function createCORSRequest(method, url) {
   var xhr = new XMLHttpRequest();
   if ("withCredentials" in xhr) {
@@ -15,9 +17,13 @@ function createCORSRequest(method, url) {
 }
 
 // Make the actual CORS request.
-function makeCorsRequest() {
+function makeCorsRequest(aIndex) {
   // This is a sample server that supports CORS.
-  var url = 'https://skyscannerplus.herokuapp.com/hello';
+  if (uniqueID == null) {
+    uniqueID = Math.floor(Math.random() * 10000000);
+  }
+
+  var url = 'https://skyscannerplus.herokuapp.com/answer/' + uniqueID + '&' + encodeURIComponent(document.getElementById("questionText").value);
 
   var xhr = createCORSRequest('GET', url);
   if (!xhr) {
@@ -28,7 +34,7 @@ function makeCorsRequest() {
   // Response handlers.
   xhr.onload = function() {
     var text = xhr.responseText;
-    alert('Response from CORS request to ' + text);
+    document.getElementById('a' + aIndex).innerHTML = text;
   };
 
   xhr.onerror = function() {
@@ -39,48 +45,3 @@ function makeCorsRequest() {
 }
 
 // Synthesizer part -----------------------
-
-// Add command (Short code artisan way)
-artyom.on(['Good morning','Good afternoon']).then(function(i){
-    switch (i) {
-        case 0:
-            artyom.say("Good morning, how are you?");
-        break;
-        case 1:
-            artyom.say("Good afternoon, how are you?");
-        break;
-    }
-});
-
-// Smart command (Short code artisan way), set the second parameter of .on to true
-artyom.on(['Repeat after me *'] , true).then(function(i,wildcard){
-    artyom.say("You've said : " + wildcard);
-});
-
-// or add some commands in the normal way
-
-artyom.addCommands([
-    {
-        indexes: ['Hello','Hi','is someone there'],
-        action: function(i){
-            artyom.say("Hello, it's me");
-        }
-    },
-    {
-        indexes: ['Repeat after me *'],
-        smart:true,
-        action: function(i,wildcard){
-            artyom.say("You've said : "+ wildcard);
-        }
-    }
-]);
-
-// Start the commands !
-artyom.initialize({
-    lang:"en-GB", // GreatBritain english
-    continuous:true, // Listen forever
-    soundex:true,// Use the soundex algorithm to increase accuracy
-    debug:true, // Show messages in the console
-    executionKeyword: "and do it now",
-    listen:true // Start to listen commands !
-});
